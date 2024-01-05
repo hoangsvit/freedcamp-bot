3b48f60ddcd24e0c1bea6051383873df5ab29eab
David Nguyen <h250694@gmail.com>
2024-01-05 16:40:58 +0700

    Refactor Freedcamp API configuration and request handling

--- app/Helper/freedcamp.php
+++ app/Helper/freedcamp.php
@@ -5,29 +5,28 @@ use Carbon\Carbon;
 
 function sendFreedcampRequest($query, $path, $method = 'get', $data = [])
 {
-    $host = config('app.freedcamp.api_url');
-    $api_key = config('app.freedcamp.api_key');
-    $api_secret = config('app.freedcamp.api_secret');
+    $config = config('freedcamp');
+
+    $host = $config['api_url'];
+    $api_key = $config['api_key'];
+    $api_secret = $config['api_secret'];
     $timestamp = Carbon::now()->timestamp;
     $hash = hash_hmac('sha1', $api_key . $timestamp, $api_secret);
-    $params = array_merge(['api_key' => $api_key, 'timestamp' => $timestamp, 'hash' => $hash], $query);
 
-    try {
-        $response = Http::withOptions([
-            'headers' => [
-                'Accept' => 'application/json',
-            ],
-            'query' => $params,
-        ]);
+    $response = Http::withOptions([
+        'headers' => ['Accept' => 'application/json'],
+        'query' => array_merge($query, [
+            'api_key' => $api_key,
+            'timestamp' => $timestamp,
+            'hash' => $hash,
+        ]),
+    ]);
 
-        $response = $method === 'get' ? $response->get("{$host}{$path}") : $response->post("{$host}{$path}", $data);
+    $response = $method === 'get' ? $response->get("{$host}{$path}") : $response->post("{$host}{$path}", $data);
 
-        $json_data = $response->json();
-        if ($response->getStatusCode() === 200 && count($json_data['data']) > 0) {
-            return $json_data;
-        }
-    } catch (Exception $e) {
-        // Handle errors if needed
+    $json_data = $response->json();
+    if (isset($json_data['data'])) {
+        return $json_data;
     }
 
     return null;
